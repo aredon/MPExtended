74d70ae5ecb7a6c4679e25fc4c14fd593fb182ac
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-12-23 21:20:10 +0100

    Implement streaming using a single method call

--- Services/MPExtended.Services.StreamingService/StreamingService.cs
+++ Services/MPExtended.Services.StreamingService/StreamingService.cs
@@ -203,6 +203,30 @@ namespace MPExtended.Services.StreamingService
         {
             return _stream.CustomTranscoderData(identifier, action, parameters);
         }
+
+        public Stream DoStream(WebStreamMediaType type, int? provider, string itemId, string clientDescription, string profileName, int startPosition)
+        {
+            // This hack only works with profiles that actually return something in the RetrieveStream method (ie no RTSP or CustomTranscoderData)
+            string identifier = String.Format("dostream-{0}", new Random().Next(10000, 99999));
+            Log.Debug("DoStream: using identifier {0}", identifier);
+
+            if (!InitStream(type, provider, itemId, clientDescription, identifier))
+            {
+                Log.Info("DoStream: InitStream() failed");
+                FinishStream(identifier);
+                return Stream.Null;
+            }
+
+            if (StartStream(identifier, profileName, startPosition).Length == 0)
+            {
+                Log.Info("DoStream: StartStream failed");
+                FinishStream(identifier);
+                return Stream.Null;
+            }
+
+            Log.Trace("DoStream: succeeded, returning stream");
+            return RetrieveStream(identifier);
+        }
         #endregion
 
         #region Images
